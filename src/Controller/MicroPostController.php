9cc021706af04315e2e22489b7db929691ecb10e
Georgi Danailov <demonhandz@gmail.com>
2025-09-04 13:16:01 +0000

    Databases and Doctrine ORM

---
+++ src/Controller/MicroPostController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\MicroPost;
+use App\Repository\MicroPostRepository;
+use DateTime;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MicroPostController extends AbstractController
+{
+    #[Route('/micro-post', name: 'app_micro_post')]
+    public function index(MicroPostRepository $posts): Response
+    {
+        return $this->render('micro_post/index.html.twig', [
+            'posts' => $posts->findAll(),
+        ]);
+    }
+
+    #[Route('/micro-post/{post}', name: 'app_micro_post_show')]
+    public function showOne(MicroPost $post): Response
+    {
+        return $this->render('micro_post/show.html.twig', [
+            'post' => $post,
+        ]);
+    }
+
+}
