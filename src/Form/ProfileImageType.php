b941cb062090ad607200383ed479d9380ca3e697
Georgi Danailov <demonhandz@gmail.com>
2025-09-23 11:06:47 +0300

    Implemented VichUploadBundle for the Avatars

--- src/Form/ProfileImageType.php
+++ src/Form/ProfileImageType.php
@@ -2,10 +2,11 @@
 
 namespace App\Form;
 
+use App\Entity\UserProfile;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 use Symfony\Component\Validator\Constraints\File;
 
 class ProfileImageType extends AbstractType
@@ -13,20 +14,23 @@ class ProfileImageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('profileImage', FileType::class, [
-                'label' => 'Profile Image (JPG or PNG file)',
-                'mapped' => false,
+            ->add('imageFile', VichImageType::class, [
                 'required' => false,
+                'allow_delete' => false,
+                'download_uri' => false,
+                'label' => 'Change Avatar',
+                'download_label' => false,
                 'constraints' => [
                     new File([
-                        'maxSize' => '1024k',
+                        'maxSize' => '2M',
                         'mimeTypes' => [
                             'image/jpeg',
                             'image/png',
+                            'image/webp',
                         ],
-                        'mimeTypesMessage' => 'Please upload a valid image file.',
+                        'mimeTypesMessage' => 'Please upload a valid image (JPEG, PNG, WEBP)',
                     ])
-                ]
+                ],
             ])
         ;
     }
@@ -34,7 +38,7 @@ class ProfileImageType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            // Configure your form options here
+            'data_class' => UserProfile::class,
         ]);
     }
 }
